ec8980f87cdfcf8075868d74975d38bcda11dd52
Ivan Botero <ivan.botero@protonmail.ch>
2020-03-06 11:55:17 -0500

    Se logra conseguir obtencion de resultados

--- Proyecto3/src/java/controller/StudentServlet.java
+++ Proyecto3/src/java/controller/StudentServlet.java
@@ -2,7 +2,6 @@ package controller;
 
 import connection.Database;
 import java.io.IOException;
-import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -18,95 +17,56 @@ import javax.servlet.http.HttpSession;
 @WebServlet(name = "StudentServlet", urlPatterns = {"/StudentServlet"})
 public class StudentServlet extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-
-        PrintWriter out = response.getWriter();
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response) {
 
         Database database;
         StudentService studentService;
 
         HttpSession session;
         RequestDispatcher view;
-        String action;
 
         database = new Database(Configuration.DATABASE_HOST, Configuration.DATABASE_USER, Configuration.DATABASE_PASSWORD, Configuration.DATABASE_NAME);
 
         if (database.connect()) {
 
+            ArrayList<Student> list;
+
             studentService = new StudentService(database);
-
             session = request.getSession();
-            action = (request.getParameter("action") != null) ? request.getParameter("action") : "";
 
-            switch (action) {
+            list = studentService.getStudent();
 
-                default:
+            try {
 
-                    ArrayList<Student> list = studentService.getStudent();
+                session.setAttribute("ACTION", "LIST");
+                session.setAttribute("RESULT", list);
 
-                    session.setAttribute("ERROR", "Mensaje");
-                    session.setAttribute("ERROR_MESSAGE", "");
+                view = request.getRequestDispatcher("student.jsp");
+                view.forward(request, response);
 
-                    session.setAttribute("RESULT", list);
-
-                    view = request.getRequestDispatcher("student.jsp");
-                    view.forward(request, response);
-
-                    break;
-
+            } catch (ServletException | IOException exception) {
+                System.err.println(exception.getMessage());
             }
 
         }
 
     }
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
     }
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
     }
 
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
     @Override
     public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
+        return "";
+    }
 
 }

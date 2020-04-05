dbc7d1169302b5ddb67aa88a4f79443216964e4b
Ivan Botero <ivan.botero@protonmail.ch>
2020-04-05 13:24:46 -0500

    Corregidos errores

---
+++ JPA/build/generated-sources/ap-source-output/entidades/Alumno_.java
@@ -0,0 +1,15 @@
+package entidades;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-04-05T13:22:44")
+@StaticMetamodel(Alumno.class)
+public class Alumno_ { 
+
+    public static volatile SingularAttribute<Alumno, Integer> codigo;
+    public static volatile SingularAttribute<Alumno, Integer> semestre;
+    public static volatile SingularAttribute<Alumno, String> nombre;
+
+}
---
+++ JPA/build/generated-sources/ap-source-output/entidades/Curso_.java
@@ -0,0 +1,17 @@
+package entidades;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-04-05T13:22:44")
+@StaticMetamodel(Curso.class)
+public class Curso_ { 
+
+    public static volatile SingularAttribute<Curso, String> descripcion;
+    public static volatile SingularAttribute<Curso, Integer> codigo;
+    public static volatile SingularAttribute<Curso, String> programa;
+    public static volatile SingularAttribute<Curso, String> profesor;
+    public static volatile SingularAttribute<Curso, Integer> creditos;
+
+}

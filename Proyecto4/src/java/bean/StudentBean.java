5f14327045851f15333c27287f48f58830dd4324
Ivan Botero <ivan.botero@protonmail.ch>
2020-04-06 12:41:53 -0500

    Correccion en P4

---
+++ Proyecto4/src/java/bean/StudentBean.java
@@ -0,0 +1,13 @@
+package bean;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+@ManagedBean
+@RequestScoped
+public class StudentBean {
+
+    public StudentBean() {
+    }
+
+}

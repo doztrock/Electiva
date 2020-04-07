fd847332bdb41bb488f582bcc92125a84699b6da
Ivan Botero <ivan.botero@protonmail.ch>
2020-04-06 20:33:30 -0500

    Proyecto4 consultando

--- Proyecto4/src/java/bean/StudentBean.java
+++ Proyecto4/src/java/bean/StudentBean.java
@@ -1,13 +1,50 @@
 package bean;
 
+import entity.Student;
+import java.util.ArrayList;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
 @ManagedBean
 @RequestScoped
 public class StudentBean {
 
+    private Student student = new Student();
+    private ArrayList<Student> list = new ArrayList<>();
+
     public StudentBean() {
+        this.load();
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    public ArrayList<Student> getList() {
+        return list;
+    }
+
+    public void setList(ArrayList<Student> list) {
+        this.list = list;
+    }
+
+    public void load() {
+
+        EntityManagerFactory entityManagerFactory;
+        EntityManager entityManager;
+
+        entityManagerFactory = Persistence.createEntityManagerFactory("PU");
+        entityManager = entityManagerFactory.createEntityManager();
+
+        this.list = new ArrayList<>(entityManager.createNamedQuery("Student.findAll", Student.class).getResultList());
+
     }
 
 }
---
+++ Proyecto4/src/java/entity/Student.java
@@ -0,0 +1,100 @@
+package entity;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@Entity
+@Table(name = "student")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Student.findAll", query = "SELECT s FROM Student s")
+    , @NamedQuery(name = "Student.findByCode", query = "SELECT s FROM Student s WHERE s.code = :code")
+    , @NamedQuery(name = "Student.findByName", query = "SELECT s FROM Student s WHERE s.name = :name")
+    , @NamedQuery(name = "Student.findBySemester", query = "SELECT s FROM Student s WHERE s.semester = :semester")})
+public class Student implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "code")
+    private Integer code;
+    @Basic(optional = false)
+    @Column(name = "name")
+    private String name;
+    @Basic(optional = false)
+    @Column(name = "semester")
+    private int semester;
+
+    public Student() {
+    }
+
+    public Student(Integer code) {
+        this.code = code;
+    }
+
+    public Student(Integer code, String name, int semester) {
+        this.code = code;
+        this.name = name;
+        this.semester = semester;
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getSemester() {
+        return semester;
+    }
+
+    public void setSemester(int semester) {
+        this.semester = semester;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (code != null ? code.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Student)) {
+            return false;
+        }
+        Student other = (Student) object;
+        if ((this.code == null && other.code != null) || (this.code != null && !this.code.equals(other.code))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "entity.Student[ code=" + code + " ]";
+    }
+
+}

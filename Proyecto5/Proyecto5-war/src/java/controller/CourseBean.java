8a9c2b713533a8b6192d9a796694c159de136b50
Ivan Botero <ivan.botero@protonmail.ch>
2020-05-08 15:47:59 -0500

    Agregado CRUD course

--- Proyecto5/Proyecto5-war/src/java/controller/CourseBean.java
+++ Proyecto5/Proyecto5-war/src/java/controller/CourseBean.java
@@ -5,6 +5,8 @@
  */
 package controller;
 
+import entity.Course;
+import java.util.List;
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
@@ -20,11 +22,43 @@ public class CourseBean {
 
     @EJB
     private CourseFacade courseFacade;
+    private Course course = new Course();
 
     /**
      * Creates a new instance of CourseBean
      */
     public CourseBean() {
+    }
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public void setCourse(Course course) {
+        this.course = course;
+    }
+
+    public String create() {
+        this.courseFacade.create(this.course);
+        return "course.xhtml";
+    }
+
+    public List<Course> read() {
+        return this.courseFacade.findAll();
+    }
+
+    public String update(Course course) {
+        this.course = course;
+        return "course.update.form.xhtml";
+    }
+
+    public String update() {
+        this.courseFacade.edit(this.course);
+        return "course.xhtml";
+    }
+
+    public void delete(Course course) {
+        this.courseFacade.remove(course);
     }
 
 }
